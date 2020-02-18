96edb88f656ad7803168fdd3d0be992bb6635c94
Martijn Cornips <cornips@users.noreply.github.com>
2020-02-18 21:06:59 +0100

    Add validateObject function to validate according to given sheme

---
+++ helpers.js
@@ -0,0 +1,33 @@
+const validateObject = (object, scheme) => {
+
+  // Validate if all required fields are provided
+  if (Object.entries(object).length !== Object.entries(scheme).length) {
+    console.error(`Object does not contain all required fields according to scheme`)
+    return false
+  }
+
+  for (let [key, value] of Object.entries(object)) {
+
+    // Check if key exists in scheme
+    if (Object.keys(scheme).indexOf(key) < 0){
+      return false
+    }
+
+    // Check if value is as expected from scheme
+    if (typeof value !== scheme[key]){
+      // typeof Array is object, so check that on first fail
+      if (scheme[key] == 'array' && Array.isArray(value)) {
+        continue;
+      }
+
+      // An inconsistency was definitely found according to scheme
+      console.error(`${key} is of type ${typeof value}, should be ${scheme[key]}`)
+      return false
+    }
+  }
+
+  // No errors until here, object is validated
+  return true
+}
+
+module.exports = validateObject;
